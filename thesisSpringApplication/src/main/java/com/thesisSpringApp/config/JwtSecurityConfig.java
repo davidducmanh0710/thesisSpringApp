73da91f3dab88b63013e82beeca8aca7497bd0c2
darklord0710 <ducmanhnguyen0710@gmail.com>
2024-05-25 20:25:21 +0700

    merge

--- thesisSpringApplication/src/main/java/com/thesisSpringApp/config/JwtSecurityConfig.java
+++ thesisSpringApplication/src/main/java/com/thesisSpringApp/config/JwtSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
